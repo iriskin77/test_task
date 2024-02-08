3a27017d39884827f0ffc1216bf9d17ab170393f
iriskin77 <talcore@yandex.ru>
2024-02-08 13:15:00 +0500

    feat: create task handler was added

--- task/db_tasks.py
+++ task/db_tasks.py
@@ -6,40 +6,39 @@ from sqlalchemy import select, and_, update, delete
 from fastapi import HTTPException
 
 
-async def _get_batch(id: int, async_session: AsyncSession):
+async def _create_task(item: TaskBase, async_session: AsyncSession):
+
+    task_check = await get_task_by_batch_date(number_batch=item.number_batch,
+                                              date_batch=item.date_batch,
+                                              async_session=async_session)
+
+    if task_check is not None:
+        query = delete(Task).where(and_(Task.number_batch == item.number_batch, item.date_batch))
+        await async_session.execute(query)
 
     try:
-        query = select(Task).where(Task.id == id)
-        task_extraced = await async_session.execute(query)
-        task = task_extraced.fetchone()[0]
+        new_task = Task(
+                 status=item.status,
+                 task=item.task,
+                 line=item.line,
+                 shift=item.shift,
+                 group=item.group,
+                 number_batch=item.number_batch,
+                 date_batch=item.date_batch,
+                 nomenclature=item.nomenclature,
+                 code=item.code,
+                 index=item.index,
+                 date_begin=item.date_begin,
+                 date_end=item.date_end)
 
-        products_query = select(Product).where(Product.number_batch_id == task.number_batch)
-        products = await async_session.execute(products_query)
+        # new_task = Task(**item.dict())
 
-        if products is None:
-            HTTPException(status_code=404, detail="Products with this batch is were not found")
-
-        res = {
-              "id": task.id,
-              "status": task.status,
-              "task": task.task,
-              "line": task.line,
-              "shift": task.shift,
-              "group": task.group,
-              "number_batch": task.number_batch,
-              "date_batch": task.date_batch,
-              "nomenclature": task.nomenclature,
-              "code": task.code,
-              "index": task.index,
-              "date_begin": task.date_begin,
-              "date_end": task.date_end,
-              "products": products.scalars()
-        }
-
-        return res
+        async_session.add(new_task)
+        await async_session.commit()
+        return new_task
 
     except Exception as ex:
-        raise HTTPException(status_code=500, detail=f"{ex}")
+        HTTPException(status_code=500, detail=f"Invalid insert to the database {ex}")
 
 
 async def get_task_by_batch_date(number_batch: int, date_batch: datetime, async_session: AsyncSession):
