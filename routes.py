76d6317f359f7815591098c5328055af961ff88e
iriskin77 <talcore@yandex.ru>
2024-02-08 15:54:54 +0500

    fix: router for products

--- routes.py
+++ routes.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from task import task_handlers
+from task.product import product_handlers
 
 
 routes = APIRouter()
 
 
 routes.include_router(router=task_handlers.router_task, prefix="/task")
+routes.include_router(router=product_handlers.router_product, prefix="/product")
