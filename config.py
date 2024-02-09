a9e4be1bea2e9099788aff36c4812582dc897ff9
iriskin77 <talcore@yandex.ru>
2024-02-09 21:32:57 +0500

    fix: change db from sqlite to postgres

--- config.py
+++ config.py
@@ -11,8 +11,6 @@ DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
 
 
-# POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
-# POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
 POSTGRES_DB = os.environ.get("POSTGRES_DB")
 POSTGRES_USER = os.environ.get("POSTGRES_USER")
 POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
--- core/settings.py
+++ core/settings.py
@@ -1,7 +1,12 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 
-SQLALCHEMY_DATABASE_URL = f"sqlite+aiosqlite:///task_db.sqlite3"
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
+engine = create_async_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False, bind=engine, class_=AsyncSession)
+
+
