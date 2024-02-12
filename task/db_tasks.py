e6148c4533bb7a86e6687de8bc41752912a2660c
iriskin77 <talcore@yandex.ru>
2024-02-12 14:22:19 +0500

    fix: style mistakes fixed according pep8, flake8

--- task/db_tasks.py
+++ task/db_tasks.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from task.models import Task, Product
-from task.schema import TaskBase, TaskFilter, TaskProducts, TaskChange
+from task.schema import TaskBase, TaskFilter
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, and_, update, delete
 from fastapi import HTTPException
@@ -10,17 +11,22 @@ from fastapi import HTTPException
 
 async def _create_task(item: TaskBase, async_session: AsyncSession):
 
-    task_check = await get_task_by_batch_date(item=item, async_session=async_session)
+    task = await get_task_by_batch_date(item=item,
+                                              async_session=async_session)
 
-    if task_check is not None:
-        query = delete(Task).where(and_(Task.number_batch == item.number_batch, Task.date_batch == item.date_batch))
+    # Если уже существует какая-то партия с аналогичным номером партии и датой партии, мы должны ее перезаписать.
+    # Здесь проверяем, если есть, то удаяем
+    if task is not None:
+        query = delete(Task).\
+            where(and_(Task.number_batch == item.number_batch,
+                       Task.date_batch == item.date_batch))
+
         await async_session.execute(query)
 
     try:
 
         new_task = Task(**item.dict())
-
-        res = TaskBase(**item.dict())
+        res = TaskBase.model_validate(item)
         async_session.add(new_task)
 
         await async_session.commit()
@@ -28,11 +34,16 @@ async def _create_task(item: TaskBase, async_session: AsyncSession):
         return res
 
     except Exception as ex:
-        HTTPException(status_code=500, detail=f"Invalid insert to the database {ex}")
+        HTTPException(status_code=500,
+                      detail=f"Invalid insert to the database {ex}")
 
 
-async def get_task_by_batch_date(item: TaskBase, async_session: AsyncSession):
-    query = select(Task).where(and_(Task.number_batch == item.number_batch, Task.date_batch == item.date_batch))
+async def get_task_by_batch_date(item: TaskBase,
+                                 async_session: AsyncSession):
+
+    query = select(Task).where(and_(Task.number_batch == item.number_batch,
+                                    Task.date_batch == item.date_batch))
+
     task = await async_session.execute(query)
     task_row = task.fetchone()
     if task_row is not None:
@@ -46,18 +57,20 @@ async def _get_task(id: int, async_session: AsyncSession):
 
     try:
 
-        task = await _get_task_by_id(id=id, async_session=async_session)
+        task = await _get_task_by_id(id=id,
+                                     async_session=async_session)
 
-        products_query = select(Product).where(Product.number_batch_id == task.number_batch)
+        products_query = select(Product).\
+            where(Product.number_batch_id == task.number_batch)
         products = await async_session.execute(products_query)
 
         if products is None:
-            HTTPException(status_code=404, detail="Products with this batch were not found")
+            HTTPException(status_code=404,
+                          detail="Products with this batch were not found")
 
         pr = [elem for elem in products.scalars()]
 
         res = {
-              "id": task.id,
               "is_closed": task.is_closed,
               "closed_at": task.closed_at,
               "task": task.task,
@@ -73,6 +86,8 @@ async def _get_task(id: int, async_session: AsyncSession):
               "date_end": task.date_end,
               "products": pr
         }
+
+        #res1 = TaskProducts.model_validate(**res)
 
         return res
 
@@ -92,23 +107,47 @@ async def _get_task_by_id(id: int, async_session: AsyncSession):
 # ================ Patch endpoint, Change a task =================
 
 
-async def _change_task(id: int, params_to_update: dict, async_session: AsyncSession):
+async def _change_task(id: int,
+                       params_to_update: dict,
+                       async_session: AsyncSession):
 
-    query = update(Task).where(Task.id == id).values(**params_to_update).returning(Task.id)
+    is_closed = params_to_update.get('is_closed', None)
+
+    # Если статус закрытия партии меняется на True, то в closed_at необходимо выставить текущий datetime
+    if is_closed is not None and is_closed:
+        params_to_update['closed_at'] = datetime.now()
+
+    # если наоборот -- то null
+    if is_closed is not None and not is_closed:
+        params_to_update['closed_at'] = None
+
+    query = update(Task).\
+        where(Task.id == id).\
+        values(**params_to_update)
+
     task_updated = await async_session.execute(query)
     await async_session.commit()
 
+    task_updated = await _get_task_by_id(id=id,
+                                 async_session=async_session)
+
     task_updated_row = task_updated.fetchone()
     if task_updated_row is not None:
-        return {"id": task_updated_row[0]}
+        return task_updated_row[0]
 
 
 # ======================== Filter tasks ==========================
 
-async def _filter_tasks(item: TaskFilter, offset: int, limit: int, async_session: AsyncSession):
+async def _get_filtered_tasks(item: TaskFilter,
+                              offset: int,
+                              limit: int,
+                              async_session: AsyncSession):
+
     params_to_sort = item.dict(exclude_none=True)
     try:
-        query = select(Task).filter_by(**params_to_sort).offset(offset).limit(limit)
+        query = select(Task).\
+            filter_by(**params_to_sort).offset(offset).limit(limit)
+
         tasks = await async_session.execute(query)
         res_tasks = [i for i in tasks.scalars()]
         res = {"tasks": res_tasks}
--- task/task_handlers.py
+++ task/task_handlers.py
@@ -4,60 +4,89 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from task import db_tasks
 from task.schema import TaskBase, TaskProducts, TaskChange, TaskFilter, TaskFilterRes, TaskChangeReturn
 
+
 router_task = APIRouter()
 
 
-@router_task.post("/")
-async def create_task(item: TaskBase, async_session: AsyncSession = Depends(get_async_session)):
+@router_task.post("/", response_model=TaskBase)
+async def create_task(item: TaskBase,
+                      async_session: AsyncSession = Depends(get_async_session)):
+
     """"Эндпойнт добавления сменных заданий"""""
+
     try:
-        res = await db_tasks._create_task(item=item, async_session=async_session)
+        res = await db_tasks._create_task(item=item,
+                                          async_session=async_session)
+
         return res
     except Exception as ex:
         raise HTTPException(status_code=500, detail=f"Database error: {ex}")
 
 
 @router_task.get("/{id}", response_model=TaskProducts)
-async def get_task(id: int, async_session: AsyncSession = Depends(get_async_session)):
+async def get_task(id: int,
+                   async_session: AsyncSession = Depends(get_async_session)):
+
     """"Эндпойнт получения сменного задания (партии) по ID (primary key)"""""
 
-    task = await db_tasks._get_task_by_id(id=id, async_session=async_session)
+    task = await db_tasks._get_task_by_id(id=id,
+                                          async_session=async_session)
 
     if task is None:
-        raise HTTPException(status_code=404, detail="Task with this id was not found")
+        raise HTTPException(status_code=404,
+                            detail="Task with this id was not found")
 
     try:
-        res = await db_tasks._get_task(id=id, async_session=async_session)
+        res = await db_tasks._get_task(id=id,
+                                       async_session=async_session)
+
         return res
     except Exception as ex:
-        raise HTTPException(status_code=500, detail=f"Database error: {ex}")
+        raise HTTPException(status_code=500,
+                            detail=f"Database error: {ex}")
 
 
 @router_task.patch("/{id}", response_model=TaskChangeReturn)
-async def change_task(id: int, params_to_update: TaskChange, async_session: AsyncSession = Depends(get_async_session)):
+async def change_task(id: int,
+                      params_to_update: TaskChange,
+                      async_session: AsyncSession = Depends(get_async_session)):
 
     """"Эндпойнт изменения сменного задания (партии) по ID (primary key)"""""
 
-    task = await db_tasks._get_task_by_id(id=id, async_session=async_session)
+    task = await db_tasks._get_task_by_id(id=id,
+                                          async_session=async_session)
+
     if task is None:
         HTTPException(status_code=404, detail="Not found")
 
     if params_to_update == {}:
-        raise HTTPException(status_code=422, detail="At least one parameter for user update info should be provided")
+        raise HTTPException(status_code=422,
+                            detail="At least one parameter for user update info should be provided")
 
     params = params_to_update.dict(exclude_none=True)
 
     try:
-        res = await db_tasks._change_task(id=id, params_to_update=params, async_session=async_session)
+        res = await db_tasks._change_task(id=id,
+                                          params_to_update=params,
+                                          async_session=async_session)
+
         return res
     except Exception as ex:
         raise HTTPException(status_code=500, detail=f"Database error: {ex}")
 
 
 @router_task.get("/filter/{item}", response_model=TaskFilterRes)
-async def get_filtered_tasks(item: TaskFilter = Depends(),  offset: int = 0, limit: int = 10, async_session: AsyncSession = Depends(get_async_session)):
-    if item == {}:
-        raise HTTPException(status_code=422, detail="At least one parameter should be provided")
+async def get_filtered_tasks(item: TaskFilter = Depends(),
+                             offset: int = 0,
+                             limit: int = 10,
+                             async_session: AsyncSession = Depends(get_async_session)):
 
-    res = await db_tasks._filter_tasks(item=item, limit=limit, offset=offset, async_session=async_session)
+    if item == {}:
+        raise HTTPException(status_code=422,
+                            detail="At least one parameter should be provided")
+
+    res = await db_tasks._get_filtered_tasks(item=item,
+                                       limit=limit,
+                                       offset=offset,
+                                       async_session=async_session)
     return res
