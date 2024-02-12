fa2319ef4aa02b4b40eaee3e88658781d023ad2f
iriskin77 <talcore@yandex.ru>
2024-02-12 14:37:37 +0500

    fix: two new functions were added to check if task and product exist in fn _product_create

--- product/db_products.py
+++ product/db_products.py
@@ -6,42 +6,78 @@ from product.schema import ProductAddTasks, ProductAggregation, ProductBase
 from fastapi import HTTPException
 
 
-async def _product_create(items: ProductAddTasks, async_session: AsyncSession):
+async def _product_create(items: ProductAddTasks,
+                          async_session: AsyncSession):
+
     products_to_add = []
-    for product in items.dict()['products']:
+    for new_product in items.dict()['products']:
 
-        query_task = select(Task).\
-            where(and_(Task.number_batch == product['number_batch_id'],
-                       Task.date_batch == product['date_product']))
-        query_unique_code = select(Product).filter(Product.unique_code == product['unique_code'])
-        task = await async_session.execute(query_task)
-        unique_code = await async_session.execute(query_unique_code)
+        task = await get_task_by_num_date_batch(number_batch_id=new_product['number_batch_id'],
+                                                date_product=new_product['date_product'],
+                                                async_session=async_session)
 
-        if task is not None:
-            if unique_code is not None:
+        product = await get_product_by_unique_code(unique_code=new_product['unique_code'],
+                                                   async_session=async_session)
 
-                new_product_to_add = {"id": product['id'],
-                                      "unique_code": product['unique_code'],
-                                      "number_batch_id": product['number_batch_id'],
-                                      "date_product": product['date_product']}
-                dict_product = Product(**new_product_to_add)
-                products_to_add.append(ProductBase(**new_product_to_add))
+        # Если продукция передана с несуществующей партией (нет сменного задания с указаным номером партии и датой партии), то данную продукцию можно игнорировать.
+        if task is None:
+            #  Если переданная продукция с данным уникальным кодом уже существует, то ее можно игнорировать.
+            if product is None:
+                new_product_to_add = {
+                            "unique_code": new_product['unique_code'],
+                            "number_batch_id": new_product['number_batch_id'],
+                            "date_product": new_product['date_product']
+                            }
 
+                dict_product = Product(**new_product)
+                products_to_add.append(ProductBase(**new_product))
                 async_session.add(dict_product)
+
+    if products_to_add == []:
+        raise HTTPException(status_code=404, detail="No products to add")
+
     await async_session.commit()
 
     return {'products': products_to_add}
 
 
-async def _aggregate_date(item: ProductAggregation, async_session: AsyncSession):
+async def get_task_by_num_date_batch(number_batch_id,
+                                     date_product,
+                                     async_session: AsyncSession):
+    query_task = select(Task).\
+            where(and_(Task.number_batch == number_batch_id,
+                       Task.date_batch == date_product))
 
-    product = await get_product_by_unique_code(item=item, async_session=async_session)
+    task = await async_session.execute(query_task)
+    task_row = task.fetchone()
+    if task_row is not None:
+        return task_row[0]
+
+
+async def get_product_by_unique_code(unique_code,
+                                     async_session: AsyncSession):
+
+    query_product = select(Product).where(Product.unique_code == unique_code)
+    product = await async_session.execute(query_product)
+    product_row = product.fetchone()
+    if product_row is not None:
+        return product_row[0]
+
+
+# ================ Aggregation data ================
+
+async def _aggregate_date(item: ProductAggregation,
+                          async_session: AsyncSession):
+
+    product = await get_product_by_unique_code(unique_code=item.unique_code,
+                                               async_session=async_session)
 
     if product is None:
         raise HTTPException(status_code=404, detail="Not Found")
 
     if product.is_aggregated:
-        raise HTTPException(status_code=400, detail=f"unique code already used at {product.is_aggregated}")
+        raise HTTPException(status_code=400,
+                            detail=f"unique code already used at {product.is_aggregated}")
 
     query_task = select(Task).where(Task.id == item.task_id)
     task = await async_session.execute(query_task)
@@ -50,7 +86,8 @@ async def _aggregate_date(item: ProductAggregation, async_session: AsyncSession)
         raise HTTPException(status_code=404, detail="Not Found")
 
     if task_check.number_batch != product.number_batch_id:
-        raise HTTPException(status_code=400, detail="unique code is attached to another batch")
+        raise HTTPException(status_code=400,
+                            detail="unique code is attached to another batch")
 
     product.is_aggregated = True
     product.aggregated_at = datetime.now()
@@ -58,10 +95,3 @@ async def _aggregate_date(item: ProductAggregation, async_session: AsyncSession)
     await async_session.commit()
     return product
 
-
-async def get_product_by_unique_code(item: ProductAggregation, async_session: AsyncSession):
-    query_product = select(Product).where(Product.unique_code == item.unique_code)
-    product = await async_session.execute(query_product)
-    product_row = product.fetchone()
-    if product_row is not None:
-        return product_row[0]
--- product/product_handlers.py
+++ product/product_handlers.py
@@ -9,21 +9,29 @@ router_product = APIRouter()
 
 
 @router_product.post("/", response_model=ProductAddTasks)
-async def product_create(items: ProductAddTasks, async_session: AsyncSession = Depends(get_async_session)):
+async def product_create(items: ProductAddTasks,
+                         async_session: AsyncSession = Depends(get_async_session)):
+
     """"Эндпойнт добавления продукции для сменного задания (партии)"""""
-    #params = item.dict(exclude_none=True)
+
     try:
-        res = await db_products._product_create(items=items, async_session=async_session)
+        res = await db_products._product_create(items=items,
+                                                async_session=async_session)
         return res
     except Exception as ex:
         raise HTTPException(status_code=500, detail=f"Database error: {ex}")
 
 
 @router_product.patch("/", response_model=ProductAggregation)
-async def aggregate(item: ProductAggregation, async_session: AsyncSession = Depends(get_async_session)):
+async def aggregate(item: ProductAggregation,
+                    async_session: AsyncSession = Depends(get_async_session)):
+
     """"Эндпойнт "аггрегации" продукции"""""
+
     try:
-        res = await db_products._aggregate_date(item=item, async_session=async_session)
+        res = await db_products._aggregate_date(item=item,
+                                                async_session=async_session)
+
         return res
     except Exception as ex:
         raise HTTPException(status_code=500, detail=f"Database error: {ex}")
